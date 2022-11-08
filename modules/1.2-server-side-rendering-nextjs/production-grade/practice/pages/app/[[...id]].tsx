2b85914c3da2b27fb88faec9e037ff8b5447e4fd
Manuel Alejandro Garisto Zuna <garistozm@outlook.com>
2022-11-07 22:30:25 -0300

    Section: Protecting pages, finished.

--- modules/1.2-server-side-rendering-nextjs/production-grade/practice/pages/app/[[...id]].tsx
+++ modules/1.2-server-side-rendering-nextjs/production-grade/practice/pages/app/[[...id]].tsx
@@ -8,7 +8,9 @@ import User from '../../components/user'
 import FolderPane from '../../components/folderPane'
 import DocPane from '../../components/docPane'
 import NewFolderDialog from '../../components/newFolderDialog'
-import { getSession, useSession } from 'next-auth/client'
+import { getSession, useSession, UserSession} from 'next-auth/client'
+
+import { connectToDB, folder, doc } from '../../db'
 
 const App: FC<{ folders?: any[]; activeFolder?: any; activeDoc?: any; activeDocs?: any[] }> = ({
   folders,
@@ -80,12 +82,34 @@ App.defaultProps = {
   folders: [],
 }
 
+// at the bottom
 export async function getServerSideProps(context) {
-  
-  const session = await getSession()
+  const session= await getSession(context)
+  // not signed in
+  if (!session || !session.user) {
+    return { props: {} }
+  }
+
+  const props: any = { session }
+  const { db } = await connectToDB()
+  const folders = await folder.getFolders(db, session.user.id)
+  props.folders = folders
+
+  if (context.params.id) {
+    const activeFolder = folders.find((f) => f._id === context.params.id[0])
+    const activeDocs = await doc.getDocsByFolder(db, activeFolder._id)
+    props.activeFolder = activeFolder
+    props.activeDocs = activeDocs
+
+    const activeDocId = context.params.id[1]
+
+    if (activeDocId) {
+      props.activeDoc = await doc.getOneDoc(db, activeDocId)
+    }
+  }
 
   return {
-      props: {session}
+    props,
   }
 }
 
