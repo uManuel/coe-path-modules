cd1944a173a1a5b1cf8b9c3dedba0f3272b6efd8
Manuel Alejandro Garisto Zuna <garistozm@outlook.com>
2023-01-02 21:54:44 -0300

    section 10: Course Project finished.

--- modules/1.9-angular-complete-guide/section-10-course-project-service-dependency-injection/src/app/app.component.ts
+++ modules/1.9-angular-complete-guide/section-10-course-project-service-dependency-injection/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RecipeService } from './recipes/recipe.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   loadedFeature = 'recipe';
+
+  constructor(private recipeService: RecipeService){
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
