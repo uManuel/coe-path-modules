a89ca32d7f07d44a566bb7219d876aa36d224731
Manuel Garisto <garistozm@outlook.com>
2023-04-20 16:05:28 -0400

    first class

--- modules/1.11-testing/frontend-master-cypress-finished/cypress/integration/01-first-flight.spec.js
+++ modules/1.11-testing/frontend-master-cypress-finished/cypress/integration/01-first-flight.spec.js
@@ -5,5 +5,15 @@ describe('Create a New Item', () => {
         cy.visit('/jetsetter');
     });
     
-    it('should');
+    it('should have a form',()=>{
+        cy.get('form').should('exist');
+    });
+
+    it('should have the words "Add Item"',()=>{
+        cy.contains('Add Item');
+    });
+
+    it('should put stuff in an input field',()=>{
+        cy.get('[data-test="new-item-input"]').type('Good attitude');
+    })
 });
