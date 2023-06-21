59b80433ecb9cecdd55edbff48c6d55846d211f0
Manuel Garisto <garistozm@outlook.com>
2023-06-21 17:05:33 -0400

    feat: Intro to Testing, Unit testing

---
+++ modules/1.12-testing-practices-principles-cucumber-bdd/testing-practices-principles/server/src/utils/__tests__/gist.todo.js
@@ -0,0 +1,15 @@
+// import {createGist} from '../gist'
+import 'regenerator-runtime/runtime';
+
+test('makes a request to the github API with the given data', async () => {
+  // const data = {
+  //   description: 'the description for this gist',
+  //   public: true,
+  //   files: {
+  //     'file1.txt': {
+  //       content: 'String file contents',
+  //     },
+  //   },
+  // }
+  // await createGist(data)
+})
