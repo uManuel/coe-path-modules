bf8de5cb2f4dba2eac2e8ec0d421779c03739dcc
Manuel Garisto <garistozm@outlook.com>
2023-06-22 16:21:06 -0400

    doc: Add comment

--- modules/1.12-testing-practices-principles-cucumber-bdd/testing-practices-principles/other/whats-a-mock/__tests__/thumb-war.2.js
+++ modules/1.12-testing-practices-principles-cucumber-bdd/testing-practices-principles/other/whats-a-mock/__tests__/thumb-war.2.js
@@ -3,6 +3,7 @@ import * as utils from '../utils'
 
 test('returns winner', () => {
   const originalGetWinner = utils.getWinner
+  //Mocking the getWinner so gets always the 2nd parameter
   utils.getWinner = (...args) => {
     utils.getWinner.mock.calls.push(args)
     return args[1]
